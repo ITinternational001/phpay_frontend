977ccbcf413dcfc1f2a3bc64b973e3e975311b06
ITinternational001 <dynastygateway03@fhbpo.com>
2025-08-02 22:54:14 +0800

    change pipeline

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    basePath: 'https://testapi.dynastypay.net',
+    basePath: 'https://api.dynastypay.net',
     test_basePath: 'https://testapi.dynastypay.net',
     APIkey: 'Default:DynastyPayApiKeyInternational2023!'
 };
